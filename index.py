7d0866b727c20541fdf2c96aba1a7e05bd7d9ddb
Garen0716 <hzg930716@gmail.com>
2023-11-12 19:51:01 +0800

    first

---
+++ index.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("autobiography.html")
+
+@app.route("/work")
+def work():
+    return render_template("work.html")
+
+@app.route("/autobiography")
+def autobiography():
+    return render_template("autobiography.html")
+app.run()
+
+# @app.route("/")
+# def web():
+#     return render_template("web.html")
+
+# @app.route("/work")
+# def main():
+#     return render_template('work.html')
+
+
+# @app.route('/')
+# def index():
+#    return render_template("about.html")
+    
+# app.run()
+
+# @app.route("/index")
+# def index():
+#     return render_template("about.html")
---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import Flask, render_template
+from waitress import serve
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    homepage = "<h1>楊子青Python網頁</h1>"
+    homepage += "<a href=/web>MIS</a><br>"
+    homepage += "<a href=/today>顯示日期時間</a><br>"
+    return homepage
+   
+
+@app.route("/web")
+def web():
+    return render_template("web.html")
+@app.route("/work")
+def work():
+    return render_template("work.html",title="work")
+
+
+
+# @app.route("/")
+# def web():
+#     return render_template("web.html")
+
+# @app.route("/work")
+# def main():
+#     return render_template('work.html')
+# if __name__ == "__main__":
+#     serve(app, host="0.0.0.0", port=8080)   
+
+# app.run()
